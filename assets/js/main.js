5f1d6aa107cc64a99db4b54e774aae8a01e0d3ef
frdevelopergen <erkinbaevbakai8@gmail.com>
2022-10-14 18:56:59 +0600

    refactor code

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,49 @@ navbarImg.addEventListener("click", function () {
 
 //===================== Navbar Close End ==========================
 
+
+
+// ======================== Show Elements Start =======================
+
+
+fetch("assets/data/coutries-card.json")
+.then(function (response) {
+  // response.json() returns a promise, use the same .then syntax to work with the results
+  response.json().then(async function  (elements) {
+    // users is now our actual variable parsed from the json, so we can use it
+   elements.forEach(element => {
+    let div = ` <div class="search-card">
+    <div class="search-default">
+      <span class="search-coutry-name">${element.country}</span>
+      <div class="search-content">
+        <div class="d-flex">
+          <span>Activities:</span>
+          <p>${element.activities}</p>
+        </div>
+        <div class="d-flex">
+          <span>Time:</span>
+          <p>${element.time}</p>
+        </div>
+      </div>
+    </div>
+    <div class="hover-search">
+      <span>${element.description}</span>
+      <a href="#">Experience this</a>
+    </div>
+  </div>`
+
+  document.querySelector('.search-card-wrapp').insertAdjacentHTML('beforeend', div)
+
+   })
+
+  });
+})
+.catch((err) => console.error(err));
+
+
+
+
+
 // ======================= Search Start =========================
 
 const form = document.getElementById("search");
@@ -27,11 +70,16 @@ const form = document.getElementById("search");
   form.addEventListener("submit", function (e) {
     // Prevent default behavior
     e.preventDefault();
+
+
+
     // Create new FormData object
     const formData = new FormData(e.target);
     let countries = $(".country").val().toString();
     let activities = $(".activities").val().toString();
     const formProps = Object.fromEntries(formData);
+
+    
 
     let searchData = {
       country: countries.toString(),
@@ -39,10 +87,17 @@ const form = document.getElementById("search");
       activities: activities.toString(),
     };
 
-    fetch("assets/data/coutries-card.json")
+    if (searchData.country !== '' || searchData.activities !== '' || searchData.date !== '') {
+      document.querySelector('.search-card-wrapp').innerHTML='';
+
+      
+
+      fetch("assets/data/coutries-card.json")
       .then(function (response) {
         // response.json() returns a promise, use the same .then syntax to work with the results
-        response.json().then(function (elements) {
+        response.json().then(async function  (elements) {
+
+          let data = [];
           // users is now our actual variable parsed from the json, so we can use it
           elements.filter(
             (element) =>
@@ -50,18 +105,46 @@ const form = document.getElementById("search");
           )
           .filter(
             (element) =>
-              element.activities?.includes(searchData.activities)
+            element.activities?.includes(searchData.activities)
           )
           .filter(
-            (element) =>
-              element.time?.includes(searchData.date)
+            (element) =>  element.time?.includes(searchData.date)
           )
           .forEach(el => {
-            console.log(el)
+            let div = ` <div class="search-card">
+            <div class="search-default">
+              <span class="search-coutry-name">${el.country}</span>
+              <div class="search-content">
+                <div class="d-flex">
+                  <span>Activities:</span>
+                  <p>${el.activities}</p>
+                </div>
+                <div class="d-flex">
+                  <span>Time:</span>
+                  <p>Nov-Dec</p>
+                </div>
+              </div>
+            </div>
+            <div class="hover-search">
+              <span>${el.description}</span>
+              <a href="#">Experience this</a>
+            </div>
+          </div>`
+        
+          document.querySelector('.search-card-wrapp').insertAdjacentHTML('beforeend', div)
           })
+
+
         });
       })
       .catch((err) => console.error(err));
+    } else {
+         
+    }
+
+  
+
+   
   });
 })();
 
