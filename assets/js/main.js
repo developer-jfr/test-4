fcc6887d69bb66d292b8c53feeb62e554fe26282
frdevelopergen <erkinbaevbakai8@gmail.com>
2022-10-14 12:13:05 +0600

    refactor code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 //====================== Navbar Close Start ========================
 
 const navItems = document.querySelectorAll(".nav-item");
@@ -20,12 +19,9 @@ navbarImg.addEventListener("click", function () {
 
 //===================== Navbar Close End ==========================
 
-
-
 // ======================= Search Start =========================
 
 const form = document.getElementById("search");
-
 
 (function () {
   form.addEventListener("submit", function (e) {
@@ -33,15 +29,40 @@ const form = document.getElementById("search");
     e.preventDefault();
     // Create new FormData object
     const formData = new FormData(e.target);
-    console.log($('.country').val().toString())
-    console.log($('.activities').val().toString())
+    let countries = $(".country").val().toString();
+    let activities = $(".activities").val().toString();
     const formProps = Object.fromEntries(formData);
-    
-    console.log(formProps)
-   
-  });
-  
-})();
 
+    let searchData = {
+      country: countries.toString(),
+      date: formProps.datatime,
+      activities: activities.toString(),
+    };
+
+    fetch("assets/data/coutries-card.json")
+      .then(function (response) {
+        // response.json() returns a promise, use the same .then syntax to work with the results
+        response.json().then(function (elements) {
+          // users is now our actual variable parsed from the json, so we can use it
+          elements.filter(
+            (element) =>
+              element.country?.includes(searchData.country)
+          )
+          .filter(
+            (element) =>
+              element.activities?.includes(searchData.activities)
+          )
+          .filter(
+            (element) =>
+              element.time?.includes(searchData.date)
+          )
+          .forEach(el => {
+            console.log(el)
+          })
+        });
+      })
+      .catch((err) => console.error(err));
+  });
+})();
 
 // ======================  Search End ========================
